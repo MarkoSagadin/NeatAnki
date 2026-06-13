1d1dd9500bf9bd29df16a8c8c607763db4789840
Marko Sagadin <marko.sagadin42@gmail.com>
2026-06-13 14:44:18 +0200

    fix: add stricter checking for the input markdown file

--- src/nanki/modules/ankicard/helpers.py
+++ src/nanki/modules/ankicard/helpers.py
@@ -1,5 +1,6 @@
 import logging
 import re
+from re import Match
 
 from nanki.modules.ankicard.utils.card_error import CardError
 
@@ -67,3 +68,53 @@ def extract_card_fields(text: str) -> dict:
         card_fields[field] = "\n".join(text_body).strip()
 
     return card_fields
+
+
+def inject_card_fields_into_template(fields: dict, template: str) -> str:
+    """Inject card fields into the card template.
+
+    Templates are expected to be valid HTML files. The replacement syntax goes like
+    this:
+
+    If HTML template has below content:
+
+    ```
+    {{Front}}
+    ```
+
+    or
+
+    ```
+    {{cloze:Front}}
+    ```
+
+    then everything is replaced by the value of the key in the card matching the text
+    inside the curly braces. Replacement is done only once, for example, if "{{Front}}"
+    is replaced with "{{Back}}", then this new string won't be replaced again, even if
+    there is a match.
+
+    That's basically it, there are some special fields though that need to be handled
+    later.
+
+    This behavior is the same as in the Anki:
+    https://docs.ankiweb.net/templates/fields.html
+    """
+    basic_fields = re.compile(r"\{\{([\w\s]+)\}\}")
+    cloze_fields = re.compile(r"\{\{cloze:([\w\s]+)\}\}")
+
+    def replace_field(m: Match[str]) -> str:
+        # m.group(0) is the whole matched word, like "{{Front}}"
+        # m.group(1) is just the "Front"
+        field = m.group(1)
+        # Replace with field's text, if field exists in the card
+        return fields.get(field, m.group(0))
+
+    template = basic_fields.sub(replace_field, template)
+
+    return cloze_fields.sub(replace_field, template)
+
+
+def get_all_fields_from_template(template: str) -> list[str]:
+    basic_fields = re.findall(r"\{\{([\w\s]+)\}\}", template)
+    cloze_fields = re.findall(r"\{\{cloze:([\w\s]+)\}\}", template)
+    return basic_fields + cloze_fields
--- src/nanki/modules/testfile.py
+++ src/nanki/modules/testfile.py
@@ -2,10 +2,13 @@ import logging
 import re
 from dataclasses import dataclass
 from pathlib import Path
-from re import Match
-from typing import Any, Self
+from typing import Self
 
 from nanki.modules.ankicard import AnkiCard
+from nanki.modules.ankicard.helpers import (
+    get_all_fields_from_template,
+    inject_card_fields_into_template,
+)
 from nanki.modules.filedata import FileData
 
 log = logging.getLogger(__name__)
@@ -34,8 +37,9 @@ class TestFile(FileData):
         test_files = []
 
         for idx, card in enumerate(cards):
+            test_files = []
             for template in templates:
-                template_fields = _get_all_fields_from_template(template.content)
+                template_fields = get_all_fields_from_template(template.content)
 
                 if not template_fields:
                     msg = (
@@ -51,17 +55,17 @@ class TestFile(FileData):
                     tags = " ".join(card.metadata.tags)
                     template.content = template.content.replace("{{Tags}}", tags)
 
-                    # Remove the Tags, so all_template_fields_found_in_all_card_fields
+                    # Remove the Tags, so _all_template_fields_found_in_all_card_fields
                     # doesn't skip the card.
                     template_fields.remove("Tags")
 
-                if not all_template_fields_found_in_all_card_fields(
+                if not _all_template_fields_found_in_all_card_fields(
                     card.fields,
                     template_fields,
                 ):
                     continue
 
-                content = _inject_card_fields_into_template(
+                content = inject_card_fields_into_template(
                     card.fields,
                     template.content,
                 )
@@ -76,6 +80,16 @@ class TestFile(FileData):
                     test_files.append(script_file)
 
                 test_files.append(cls(path, content))
+
+            # If the set of card fields is bigger than every set of template fields then
+            # that means that user created a card that can't be matched to any html
+            # template.
+            all_t_fields = [get_all_fields_from_template(t.content) for t in templates]
+            num_card_fields = len(set(card.fields.keys()))
+
+            if all(num_card_fields > len(set(t_field)) for t_field in all_t_fields):
+                msg = "Fields in the parsed card don't match any found html template!"
+                raise UserWarning(msg)
 
         return test_files
 
@@ -138,47 +152,8 @@ def _add_js_script(content: str, script: str) -> tuple[str, TestFile]:
     return content, script_file
 
 
-def _inject_card_fields_into_template(fields: dict, template: str) -> str:
-    """Inject card fields into the card template.
-
-    Templates are expected to be valid HTML files. The replacement syntax goes like
-    this:
-
-    If HTML template has below content:
-
-    ```
-    {{Front}}
-    ```
-
-    then everything is replaced by the value of the key in the card matching the text
-    inside the curly braces. Replacement is done only once, for example, if "{{Front}}"
-    is replaced with "{{Back}}", then this new string won't be replaced again, even if
-    there is a match.
-
-    That's basically it, there are some special fields though that need to be handled
-    later.
-
-    This behavior is the same as in the Anki:
-    https://docs.ankiweb.net/templates/fields.html
-    """
-    pattern = re.compile(r"\{\{([\w\s]+)\}\}")
-
-    def replace_field(m: Match[str]) -> str:
-        # m.group(0) is the whole matched word, like "{{Front}}"
-        # m.group(1) is just the "Front"
-        field = m.group(1)
-        # Replace with field's text, if field exists in the card
-        return fields.get(field, m.group(0))
-
-    return pattern.sub(replace_field, template)
-
-
-def all_template_fields_found_in_all_card_fields(
+def _all_template_fields_found_in_all_card_fields(
     card_fields: dict,
     template_fields: list[str],
 ) -> bool:
     return set(template_fields).issubset(set(card_fields.keys()))
-
-
-def _get_all_fields_from_template(template: str) -> list[Any]:
-    return re.findall(r"\{\{([\w\s]+)\}\}", template)
